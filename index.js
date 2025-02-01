e78129a5e8d93f864282569015a27459d67dca6a
Achint115 <achintsing1155@gmail.com>
2025-02-01 13:56:10 +0530

    api updated

--- index.js
+++ index.js
@@ -15,6 +15,9 @@ app.use(cors({
 }))
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
+app.get("/", (req, res)=>{
+  res.json({message: "Server is live"})
+})
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
 });
